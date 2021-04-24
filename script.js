92da65611d627a25c03a6cb9eff12b08fbb9ffb8
Godreigneth <godreignethlevidibia@yahoo.com>
2021-04-24 20:49:03 +0000

    Created the toggleNav Function

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function navAnimation(direction1, direction2) {
     nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`);
   });
 }
+
+function toggleNav() {
+  // Toggle: Menu Bars Open/Closed
+  menuBars.classList.toggle('change');
+  // Toggle: Menu Active
+  overlay.classList.toggle('overlay-active');
+  if (overlay.classList.contains('overlay-active')) {
+    // Animate In - Overlay
+    overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+    // Animate In - Nav Items
+    navAnimation('out', 'in');
+  } else {
+    // Animate Out - Overlay
+    overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+    // Animate Out - Nav Items
+    navAnimation('in', 'out');
+  }
+}
